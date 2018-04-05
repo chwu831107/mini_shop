e3eee91fc7e433d2767c065b465d7085562a5291
C.H Wu <ice831107@gmail.com>
2018-04-05 15:37:29 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('_SHOP_NAME', '鳳山大哥大');
+define('_DB_HOST', 'localhost');
+define('_DB_ID', 'root');
+define('_DB_PW', '525566520');
+define('_DB_NAME', 'mini_shop');
+define('_UPLOAD_PATH', 'C:/UniServerZ/www/mini_shop/uploads/');
+define('_UPLOAD_URL', 'http://localhost//mini_shop/uploads/');
+
+$admin_array = ['a123'];
